da81397dc5818560568700c1d1199bb9122030cf
Jrana1 <jewelbutex12@gmail.com>
2021-08-28 21:41:15 +0200

    done

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
+let inputField = document.getElementById("input-field");
 function getApi() {
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=Frankfurt&units=metric&appid=e2baf0e407b523dfeb68b094cded715f")
-
-
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&units=metric&appid=e2baf0e407b523dfeb68b094cded715f`)
         .then(res => res.json())
         .then(data => displayWeather(data));
+    document.getElementById("input-field").value = "";
 };
-
 function displayWeather(data) {
-    console.log(`${data.main.temp}°C`);
+    console.log(data);
+    document.getElementById("name").innerText = data.name;
+    document.getElementById("temp").innerText = `${data.main.temp}°C`;
+
 }
